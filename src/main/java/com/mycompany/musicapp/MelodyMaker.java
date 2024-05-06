303a6f86d55fd4660fbd12ebbaf9a4471b497974
TroyDeminico <tdeminico1@mail.endicott.edu>
2024-05-05 21:20:47 -0400

    name fix

--- src/main/java/com/mycompany/musicapp/MelodyMaker.java
+++ src/main/java/com/mycompany/musicapp/MelodyMaker.java
@@ -17,7 +17,7 @@ import javax.swing.JComboBox;
  *
  * @author troydeminico\
  */
-public class melodyMaker extends javax.swing.JFrame {
+public class MelodyMaker extends javax.swing.JFrame {
     
     Playlist userPlaylist = new Playlist();
     Playlist favPlaylist = new Playlist();
@@ -93,7 +93,7 @@ public class melodyMaker extends javax.swing.JFrame {
     /**
      * Creates new form NewJFrame
      */
-    public melodyMaker() {
+    public MelodyMaker() {
         initComponents(); 
     }
 
@@ -493,14 +493,18 @@ public class melodyMaker extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(melodyMaker.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MelodyMaker.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(melodyMaker.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MelodyMaker.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(melodyMaker.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MelodyMaker.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(melodyMaker.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(MelodyMaker.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
         //</editor-fold>
@@ -509,7 +513,7 @@ public class melodyMaker extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new melodyMaker().setVisible(true);
+                new MelodyMaker().setVisible(true);
             }
         });
     }
